8f6b09ee9aaaa4aad74715095cbdd9b198adc7e4
Thomas Basham <Bashamtg@gmail.com>
2023-06-27 18:19:28 -0700

    added function for guessing game

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function getUserName() {
-  let username = prompt("Hello! What is your name?");
+  let username = prompt("What is your name?");
   return document.write(username);
 }
 
@@ -29,8 +29,31 @@ function getFinalMessage() {
       "You probably know most of this already since you have " +
       yearsExperience +
       " of coding, but hopefully something was helpful!";
+  } else {
+    yearsExperience = prompt(
+      "How many years of experience coding do you have?"
+    );
   }
-  
-  document.write(message);
+
+  return document.write(message);
 }
 
+function guessJavaScriptDate() {
+  let userWantsToPlay = confirm("Do you want to play a trivia game?");
+  if (userWantsToPlay == true) {
+    let guess;
+    let correctAnswer = 26;
+    while (guess != correctAnswer) {
+      guess = prompt("How old is the programming JavaScript?");
+      if (guess < correctAnswer) {
+        alert("Too low! Guess again");
+      } else if (guess > correctAnswer) {
+        alert("Too High! Guess again.");
+      } else
+        alert(
+          "That's correct! JavaScript was invented by Brendan Eich in 1995, making it 26 years old!"
+        );
+    }
+  }
+}
+guessJavaScriptDate()
